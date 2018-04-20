a9f6d9c43e0811d98075923715f895071e24803e
shzshz7 <shzshz777@gmail.com>
2018-04-20 00:11:31 -0400

    first express app

--- users/server.js
+++ users/server.js
@@ -1,7 +1,13 @@
-import express from "express";
+// import express from "express";
+const express = require('express');
+// import expressGraphQL from "express-graphql";
+const expressGraphQL = require('express-graphql');
 
 const app = express();
 
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
 
 
 app.listen(4000, () => {
