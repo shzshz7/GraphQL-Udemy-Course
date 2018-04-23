71b0991140ad5666074040fe899731daa1a479bb
shzshz7 <shzshz777@gmail.com>
2018-04-23 16:20:19 -0400

    starting Bidirection section

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-// const _ = require('lodash');
+// const _ = require('lodash'); dont need lodash anymore since we dont use the static data anymore
 const axios = require('axios');
 const {
     GraphQLObjectType,
@@ -13,12 +13,29 @@ const {
 //     {id: '47', firstName: 'Samantha', age: 21}
 // ]
 
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString}
+    }
+});
+
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
-        age: {type: GraphQLInt}
+        age: {type: GraphQLInt},
+        company: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                // console.log(parentValue, args);
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                .then(res => res.data);
+            }
+        }
     }
 });
 
@@ -30,7 +47,15 @@ const RootQuery = new GraphQLObjectType({
             args: { id: {type: GraphQLString}},
             resolve(parentValue, args) {
                 // return _.find(users, {id: args.id});
-                return axios.get(`http://localhost:3000/users/${args.id}`)
+                return axios.get(`http://localhost:3000/users/${args.id}`)//axios.get return a Promise
+                .then(response => response.data);
+            }
+        },
+        company: {
+            type: CompanyType,
+            args: { id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${args.id}`)//axios.get return a Promise
                 .then(response => response.data);
             }
         }
